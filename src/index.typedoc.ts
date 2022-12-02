8551d42934e9f77dc0204f72652f263e93b37924
dyh_a <xinzon32177@163.com>
2022-12-02 16:12:00 +0800

    chore: update typedoc

---
+++ src/index.typedoc.ts
@@ -0,0 +1,20 @@
+/**
+ * 与数据类型相关的工具函数
+ */
+export * as DataType from './data-type';
+/**
+ * 数组相关工具函数
+ */
+export * as Array from './array';
+/**
+ * 异步数组工具函数
+ */
+export * as ArrayAsync from './array-async';
+/**
+ * 对象相关工具函数
+ */
+export * as Object from './object';
+/**
+ * Promise相关工具函数
+ */
+export * as Promise from './promise';
