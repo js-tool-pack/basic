a14c5c220218a67cc8c8f1f02bfbcd9f37c375e6
dyh_a <xinzon32177@163.com>
2022-12-13 22:15:37 +0800

    refactor(time): `formatDate`
    1.添加可选项季节月份范围`seasonRanges`
    
    BREAKING CHANGE:
    1.seasonText改为seasonNames
    2.weekText改为weekNames
    3.季节月份范围不再默认为1-3，4-6...

--- src/time.ts
+++ src/time.ts
@@ -49,9 +49,9 @@ export function msToDateStr(ms: number, format = 'd天hh时mm分ss秒') {
  * formatDate(date1, 'yyyy-MM-dd'); // '2020-02-02'
  * formatDate(date1, 'hh:mm:ss'); // '10:10:10'
  * formatDate(date1, 'dd-MM-yyyy'); // '02-02-2020'
- * formatDate(date1, 'yyyyq季'); // '2020春季'
+ * formatDate(date1, 'yyyyq季'); // '2020冬季'
  *
- * // week start
+ * // 星期
  * formatDate(new Date('2020-01-12'), '周w'); // '周日'
  * formatDate(new Date('2020-01-12'), 'w'); // '日'
  * formatDate(new Date('2020-01-13'), 'w'); // '一'
@@ -60,46 +60,60 @@ export function msToDateStr(ms: number, format = 'd天hh时mm分ss秒') {
  * formatDate(new Date('2020-01-16'), 'w'); // '四'
  * formatDate(new Date('2020-01-17'), 'w'); // '五'
  * formatDate(new Date('2020-01-18'), 'w'); // '六'
- * // week end
  *
- * // season start
- * formatDate(new Date('2020-01-12'), 'q'); // '春'
- * formatDate(new Date('2020-02-12'), 'q'); // '春'
+ * // 季节
+ * formatDate(new Date('2020-01-12'), 'q'); // '冬'
+ * formatDate(new Date('2020-02-12'), 'q'); // '冬'
  * formatDate(new Date('2020-03-13'), 'q'); // '春'
- * formatDate(new Date('2020-04-14'), 'q'); // '夏'
- * formatDate(new Date('2020-05-15'), 'q'); // '夏'
+ * formatDate(new Date('2020-04-14'), 'q'); // '春'
+ * formatDate(new Date('2020-05-15'), 'q'); // '春'
  * formatDate(new Date('2020-06-16'), 'q'); // '夏'
- * formatDate(new Date('2020-07-17'), 'q'); // '秋'
- * formatDate(new Date('2020-08-18'), 'q'); // '秋'
+ * formatDate(new Date('2020-07-17'), 'q'); // '夏'
+ * formatDate(new Date('2020-08-18'), 'q'); // '夏'
  * formatDate(new Date('2020-09-18'), 'q'); // '秋'
- * formatDate(new Date('2020-10-18'), 'q'); // '冬'
- * formatDate(new Date('2020-11-18'), 'q'); // '冬'
+ * formatDate(new Date('2020-10-18'), 'q'); // '秋'
+ * formatDate(new Date('2020-11-18'), 'q'); // '秋'
  * formatDate(new Date('2020-12-18'), 'q'); // '冬'
  *
  * // 自定义季节名
- * formatDate(new Date('2020-01-12'), 'q', { seasonText: ['spring'] }); // 'spring'
+ * const seasonNames = ['spring', 'summer', 'autumn', 'winter'];
+ * formatDate(new Date('2020-01-12'), 'q', { seasonNames }); // 'winter'
+ *
+ * // 自定义季节所在月份范围
+ * const seasonRanges: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
+ * formatDate(new Date('2020-01-12'), 'q', { seasonNames, seasonRanges }); // 'spring'
+ * formatDate(new Date('2020-12-12'), 'q', { seasonNames, seasonRanges }); // 'winter'
  *
  * // 自定义星期名字
- * const weekText = ['sunday', 'monday'];
- * formatDate(new Date('2020-01-12'), 'w', { weekText }); // 'sunday'
+ * const weekNames = ['sunday', 'monday'];
+ * formatDate(new Date('2020-01-12'), 'w', { weekNames }); // 'sunday'
  *
- * @param [formular="yyyy-MM-dd hh:mm:ss"] 格式，默认：'yyyy-MM-dd hh:mm:ss'
+ * @param [formular='yyyy-MM-dd hh:mm:ss']
+ * @param formular 格式，默认：'yyyy-MM-dd hh:mm:ss'
  * @param date 日期
  * @param options 可选选项
- * @param options.seasonText 季节名数组，默认：['春', '夏', '秋', '冬']
- * @param options.weekText 星期名数组，默认：['日', '一', '二', '三', '四', '五', '六']
+ * @param options.seasonNames 季节名数组，默认：['春', '夏', '秋', '冬']
+ * @param options.seasonRanges 季节所在月份范围，默认：春季为3-5月，夏季为6-8月、秋季为9-11月、冬季为12-2月
+ * @param options.weekNames 星期名数组，默认：['日', '一', '二', '三', '四', '五', '六']
  */
 export function formatDate(
   date: Date,
   formular = 'yyyy-MM-dd hh:mm:ss',
   options?: {
-    seasonText?: string[];
-    weekText?: string[];
+    seasonNames?: string[];
+    seasonRanges?: number[][];
+    weekNames?: string[];
   },
 ) {
   const opt = {
-    weekText: ['日', '一', '二', '三', '四', '五', '六'],
-    seasonText: ['春', '夏', '秋', '冬'],
+    weekNames: ['日', '一', '二', '三', '四', '五', '六'],
+    seasonNames: ['春', '夏', '秋', '冬'],
+    seasonRanges: [
+      [3, 4, 5],
+      [6, 7, 8],
+      [9, 10, 11],
+      [12, 1, 2],
+    ],
     ...options,
   };
   const o: Record<string, Function> = {
@@ -110,12 +124,14 @@ export function formatDate(
     's+': () => date.getSeconds(), //秒
     q: () => {
       //季度
-      // 按月份区分的季度并不准确
-      const q = Math.floor((date.getMonth() + 3) / 3) - 1;
-      return opt.seasonText[q];
+      // 按月份区分的季度并不真的准确
+      // 春季为3-5月，夏季为6-8月、秋季为9-11月、冬季为12-2月
+      const month = date.getMonth() + 1;
+      const qIndex = opt.seasonRanges.findIndex((range) => range.includes(month));
+      return opt.seasonNames[qIndex];
     },
     'S+': () => date.getMilliseconds(), //毫秒
-    w: () => opt.weekText[date.getDay()], //周
+    w: () => opt.weekNames[date.getDay()], //周
   };
   if (/(y+)/.test(formular)) {
     const y = RegExp.$1;
@@ -124,15 +140,15 @@ export function formatDate(
       String(date.getFullYear()).padStart(y.length, '0').slice(-y.length),
     );
   }
+  let result = formular;
   for (const k in o) {
     if (new RegExp('(' + k + ')').test(formular)) {
       const s1 = RegExp.$1;
-      const v = String(o[k]?.());
-      // const value = s1.length === 1 ? v : ("00" + v).substr(String(v).length);
-      formular = formular.replace(s1, v.padStart(s1.length, '0'));
+      const v = String((o[k] as Function)());
+      result = result.replace(s1, v.padStart(s1.length, '0'));
     }
   }
-  return formular;
+  return result;
 }
 
 /**
