14085d84b9e233257f6e278ab16fd39782ee6a34
dyh_a <xinzon32177@163.com>
2022-12-20 14:46:26 +0800

    refactor(bezier.ts): 使用目录和文件分组

---
+++ src/bezier/base.ts
@@ -0,0 +1,11 @@
+/**
+ * 2阶贝塞尔曲线
+ *
+ * @param t 百分比 [0, 1]
+ * @param v1 值1
+ * @param cv 控制值
+ * @param v2 值2
+ */
+export function bezier2(t: number, v1: number, cv: number, v2: number): number {
+  return (1 - t) * (1 - t) * v1 + 2 * t * (1 - t) * cv + t * t * v2;
+}
--- src/bezier/point.ts
+++ src/bezier/point.ts
@@ -1,18 +1,5 @@
-import { Point } from './coordinate';
-
-// t是百分比，a是参数
-
-/**
- * 2阶贝塞尔曲线
- *
- * @param t 百分比 [0, 1]
- * @param v1 值1
- * @param cv 控制值
- * @param v2 值2
- */
-export function bezier2(t: number, v1: number, cv: number, v2: number): number {
-  return (1 - t) * (1 - t) * v1 + 2 * t * (1 - t) * cv + t * t * v2;
-}
+import { Point } from '../coordinate';
+import { bezier2 } from './base';
 
 /**
  * 生成二阶贝塞尔曲线路径点
