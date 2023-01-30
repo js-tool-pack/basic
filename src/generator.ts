ca8a90a14ce3a1569c3b4419997b8bd277371a72
dyh_a <xinzon32177@163.com>
2023-01-30 16:14:30 +0800

    feat(generator): createTimeCountUpGen--createTimeCountUp的Generator版本

--- src/generator.ts
+++ src/generator.ts
@@ -92,3 +92,85 @@ export function* idGen(
     id += _step;
   }
 }
+
+/**
+ * 创建一个时间累计生成器
+ * ---
+ * createTimeCountUp的Generator版本
+ * @see createTimeCountUp
+ *
+ * @example
+ * const t = timeCountUpGen();
+ *
+ * t.next().value; // 0
+ *
+ * await sleep(20);
+ * t.next().value; // 20 <= t.next().value <= 30
+ *
+ * await sleep(30);
+ * const beforePause = t.next().value; // 50 <= t.next().value <= 60
+ *
+ * // 暂停
+ * t.next(false);
+ * await sleep(10);
+ * t.next().value === beforePause; // true
+ * t.next().value; // 50 <= t.next().value <= 60
+ *
+ * // 继续
+ * t.next(true);
+ * await sleep(10);
+ * t.next().value; // 60 <= t.next().value <= 70
+ *
+ * // 停止
+ * t.return();
+ * t.next() // { done: true, value: undefined }
+ *
+ * @example
+ * // 使用for...of
+ * async function test(){
+ *   let count = 0;
+ *   const t = timeCountUpGen();
+ *   for(const v of t){
+ *     console.log(v);
+ *     await sleep(1000);
+ *     count++ >= 10 && t.return();
+ *   }
+ * }
+ * test();
+ * // outputs
+ * // 1010
+ * // 2015
+ * // 3024
+ * // 4031
+ * // 5040
+ * // 6048
+ * // 7057
+ * // 8065
+ * // 9073
+ * // 10081
+ */
+export function createTimeCountUpGen(): Generator<number, void, boolean | void> {
+  const startTime = Date.now();
+  const pauseState = {
+    total: 0,
+    startTime: 0,
+  };
+
+  function pause(): void {
+    pauseState.startTime = Date.now();
+  }
+  function play(): void {
+    pauseState.total += Date.now() - pauseState.startTime;
+    pauseState.startTime = 0;
+  }
+
+  function* g(): ReturnType<typeof createTimeCountUpGen> {
+    while (true) {
+      const endTime = pauseState.startTime ? pauseState.startTime : Date.now();
+      const isPlay = yield endTime - startTime - pauseState.total;
+      if (isPlay === void 0) continue;
+      isPlay ? play() : pause();
+    }
+  }
+  return g();
+}
