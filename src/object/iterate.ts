55bdc84603435abd14e9e7a175c57eb83130dd10
dyh_a <xinzon32177@163.com>
2023-03-12 04:26:03 +0800

    feat(object): replaceValues

--- src/object/iterate.ts
+++ src/object/iterate.ts
@@ -48,6 +48,29 @@ export function forEachObj<T extends object>(
 }
 
 /**
+ * 替换数组或对象的value
+ *
+ * @example
+ *
+ * const obj = { a: 1, b: 2 };
+ * replaceValues(obj, String); // { a: '1', b: '2' }
+ * console.log(obj); // { a: '1', b: '2' }
+ *
+ * const arr = [1, 2, 3];
+ * replaceValues(arr, String); // ['1', '2', '3']
+ * console.log(arr); // ['1', '2', '3']
+ */
+export function replaceValues<T extends Record<string, unknown> | Array<unknown>>(
+  target: T,
+  replacement: (v: unknown, k: string, target: T) => unknown,
+) {
+  forEachObj(target, (v, k): void => {
+    target[k as string] = replacement(v, k as string, target);
+  });
+  return target;
+}
+
+/**
  *
  * 代替Object.keys(obj).reduce
  *
