fbd26334ea9031f88f8675e0f5f83e45cc9eb5f8
dyh_a <xinzon32177@163.com>
2023-03-28 14:06:33 +0800

    test(string): getClassNames对象类型参数不再限制值的类型

--- __tests__/string.test.ts
+++ __tests__/string.test.ts
@@ -243,8 +243,10 @@ describe('string', function () {
     expect(getClassNames('a', 'b')).toBe('a b');
     expect(getClassNames({ a: true, b: false, c: true })).toBe('a c');
     expect(getClassNames('a', 'b', { a: true, b: false, c: true })).toBe('a b c');
-    expect(getClassNames('  ', 'a', '   ', 'b', '     ', 'c', { a: true, b: false, c: true })).toBe(
+    expect(getClassNames('  ', 'a', '   ', 'b', '     ', 'c', { a: true, b: '', c: true })).toBe(
       'a b c',
     );
+    expect(getClassNames({ a: true, b: '  ', c: '' })).toBe('a b');
+    expect(getClassNames({ a: true, b: 1, c: 0 })).toBe('a b');
   });
 });
