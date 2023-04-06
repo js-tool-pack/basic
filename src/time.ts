128cb9455644776e14e4d8e0daeb1619e25d1a90
dyh_a <xinzon32177@163.com>
2023-04-07 07:39:21 +0800

    feat(time): getTimePeriodConst 获取时间段常数

--- src/time.ts
+++ src/time.ts
@@ -641,3 +641,35 @@ export function calcRelativeDate(init: Date) {
   const diff = init.getTime() - Date.now();
   return () => new Date(Date.now() + diff);
 }
+
+/**
+ * 获取时间段常数
+ *
+ * @returns return {
+ *   millisecond: 1,
+ *   second: 1000,
+ *   minute: 60 * 1000,
+ *   hour: 60 * 60 * 1000,
+ *   day: 24 * 60 * 60 * 1000,
+ *   week: 7 * 24 * 60 * 60 * 1000,
+ *   month: 30 * 24 * 60 * 60 * 1000,
+ *   season: 3 * 30 * 24 * 60 * 60 * 1000,
+ *   year: 12 * 30 * 24 * 60 * 60 * 1000,
+ * }
+ */
+export const getTimePeriodConst = (function () {
+  const millisecond = 1;
+  const second = millisecond * 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const month = day * 30;
+  const season = month * 3;
+  const year = month * 12;
+  const week = day * 7;
+
+  const timeConst = { millisecond, second, minute, hour, day, week, month, season, year } as const;
+  return function () {
+    return timeConst;
+  };
+})();
