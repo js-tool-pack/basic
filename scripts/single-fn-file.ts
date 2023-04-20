0e03f14c015e23c2e18729acb3e88ac872ca4122
dyh_a <xinzon32177@163.com>
2023-04-20 17:37:04 +0800

    chore(scripts): add single-fn-file.ts；拆分多函数文件为单函数文件

---
+++ scripts/single-fn-file.ts
@@ -0,0 +1,147 @@
+import * as Path from 'path';
+import * as FS from 'fs';
+import type {
+  Declaration,
+  ExportNamedDeclaration,
+  FunctionDeclaration,
+  VariableDeclaration,
+} from '@babel/types';
+import { parse } from '@babel/parser';
+import generate from '@babel/generator';
+import { prompt } from 'enquirer';
+import { Identifier } from '@babel/types';
+import chalk from 'chalk';
+
+/**
+ * 把一个大函数集合文件转成单函数文件
+ */
+async function run() {
+  console.log('*'.repeat(10), '大函数集合文件转成单函数文件', '*'.repeat(10));
+  try {
+    const filepath = await selectFile(Path.resolve(__dirname, '..'));
+
+    const functions = extractFunctionsFromFile(filepath);
+    console.log(
+      `\n提取了 ${chalk.green(functions.length)} 个函数:\n`,
+      chalk.green(JSON.stringify(functions, null, 2)),
+    );
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+/**
+ * 大函数集合文件转成单函数文件
+ */
+function extractFunctionsFromFile(filepath: string): string[] {
+  const fileContent = FS.readFileSync(filepath, 'utf-8');
+  // 解析文件内容为 AST
+  const ast = parse(fileContent, { sourceType: 'module', plugins: ['typescript'] });
+
+  // 提取函数定义
+  const nodes = ast.program.body.filter(
+    (node) => node.type === 'ExportNamedDeclaration',
+  ) as ExportNamedDeclaration[];
+
+  const dirname = Path.dirname(filepath);
+
+  function getFnName(declaration: Declaration): string {
+    switch (declaration.type) {
+      case 'FunctionDeclaration':
+        return (declaration as FunctionDeclaration).id!.name;
+      case 'VariableDeclaration':
+        return ((declaration as VariableDeclaration).declarations[0]!.id as Identifier).name;
+      case 'ExportNamedDeclaration':
+        return getFnName((declaration as ExportNamedDeclaration).declaration!);
+      default:
+        console.error(declaration);
+        throw new Error(`未识别的类型：${declaration.type}`);
+    }
+  }
+
+  // 创建文件流
+  const indexStream = FS.createWriteStream(Path.resolve(dirname, `index.ts`), 'utf-8');
+  nodes.forEach((node) => {
+    // 生成单独的函数文件内容
+    const functionFileContent = generate({
+      ...node,
+      // 去除代码后面的注释
+      trailingComments: [],
+    }).code;
+
+    // 获取函数名
+    const filename = getFnName(node.declaration!);
+
+    // 生成单独的函数文件
+    FS.writeFileSync(Path.resolve(dirname, filename + '.ts'), functionFileContent + '\n');
+
+    // 写入流 index.ts导出
+    indexStream.write(`export * from './${filename}';\n`);
+  });
+  // 关闭流
+  indexStream.end();
+
+  // 返回函数名列表
+  return nodes.map(getFnName);
+}
+
+const ignoreList = [
+  /^\./,
+  /\.(md|json|js|tsbuildinfo|yaml|css)$/,
+  'dist',
+  'node_modules',
+  '__tests__',
+  'docs-html',
+  'dist',
+  'scripts',
+  'temp',
+  'LICENSE',
+];
+
+/**
+ * 选择文件
+ */
+async function selectFile(rootPath: string): Promise<string> {
+  const paths = FS.readdirSync(rootPath)
+    .filter(
+      (path) =>
+        !ignoreList.some((ignore) =>
+          typeof ignore === 'string' ? ignore === path : ignore.test(path),
+        ),
+    )
+    .map((name) => {
+      const path = Path.resolve(rootPath, name);
+      const isFile = FS.statSync(path).isFile();
+      return { name, path, isFile };
+    })
+    .sort((a, b) => Number(a.isFile) - Number(b.isFile));
+
+  const { path } = await prompt<{ path: string }>([
+    {
+      type: 'select',
+      name: 'path',
+      message: `选择文件`,
+      choices: paths.map<{ name: string; hint: string }>(({ name, path, isFile }) => {
+        return {
+          message: name,
+          name: path,
+          hint: isFile ? 'file' : 'dir',
+        };
+      }),
+    },
+  ]);
+
+  if (!paths.find((p) => p.path === path)!.isFile) return await selectFile(path);
+
+  const { confirm } = await prompt<{ confirm: boolean }>({
+    type: 'confirm',
+    name: 'confirm',
+    message: `确定转换该文件(${path})？`,
+  });
+
+  if (!confirm) throw '取消选择文件....';
+
+  return path;
+}
+
+run();
