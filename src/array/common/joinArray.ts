f9f1b172caa693d864d09c9bd5b3a80039099cc8
dyh_a <xinzon32177@163.com>
2023-06-19 05:19:39 +0800

    feat(array): joinArray添加callbackFn回调，可对数组遍历进行操作

--- src/array/common/joinArray.ts
+++ src/array/common/joinArray.ts
@@ -7,7 +7,7 @@ import { isFunction } from '../../data-type';
  * 与join合成string不同的是joinToArray是加入到数组中，
  * 该方法不对原数组操作
  *
- * @override separator为固定值
+ * @override ## separator为固定值
  *
  * @example
  *
@@ -17,30 +17,39 @@ import { isFunction } from '../../data-type';
  * joinArray<number | string>([1, 2, 3], 'a'); // [1, 'a', 2, 'a', 3]
  *
  */
-export function joinArray<T>(arr: ArrayLike<T>, separator: T): T[];
+export function joinArray<T>(
+  arr: ArrayLike<T>,
+  separator: T,
+  callbackFn?: (item: T, index: number) => T,
+): T[];
 /**
- * 添加元素到数组缝隙中
- *
- * 类似Array.prototype.join，
- * 与join合成string不同的是joinToArray是加入到数组中，
- * 该方法不对原数组操作
- *
- * @override separator为回调函数
+ * @override ## separator为回调函数
  *
  * @example
  *
  * joinArray([1, 2, 3], (i) => i + 10); // [1, 11, 2, 13, 3]
  *
  */
-export function joinArray<T>(arr: ArrayLike<T>, separator: (index: number) => T): T[];
-export function joinArray(arr: ArrayLike<unknown>, separator: unknown): unknown[] {
+export function joinArray<T>(
+  arr: ArrayLike<T>,
+  getSeparator: (index: number) => T,
+  callbackFn?: (item: T, index: number) => T,
+): T[];
+export function joinArray(
+  arr: ArrayLike<unknown>,
+  separator: unknown,
+  callbackFn: (item: unknown, index: number) => unknown = (it) => it,
+): unknown[] {
   const len = arr.length;
   if (!len) return [];
 
-  const res: unknown[] = [arr[0]];
+  const res: unknown[] = [callbackFn(arr[0], 0)];
 
   const cb = isFunction<(index: number) => unknown>(separator) ? separator : () => separator;
-  for (let i = 1; i < len; i++) res.push(cb(res.length), arr[i]);
+  for (let i = 1; i < len; i++) {
+    const index = res.length;
+    res.push(cb(index), callbackFn(arr[i], index + 1));
+  }
 
   return res;
 }
