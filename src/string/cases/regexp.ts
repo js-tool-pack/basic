9eac0085a2e45610250968e017f7f957c9de35c1
dyh_a <xinzon32177@163.com>
2023-07-01 00:06:17 +0800

    docs(string): update CaseSplitRegExp

--- src/string/cases/regexp.ts
+++ src/string/cases/regexp.ts
@@ -1,4 +1,7 @@
 /**
  * 变量命名法通用切割正则
+ *
+ * ascii标点字符定义见该页面
+ * @see {@link https://www.kerryr.net/pioneers/ascii3.htm}
  */
 export const CaseSplitRegExp = /[\s!-/:-@[-`{-~]+|(?<![A-Z])(?=[A-Z])/g;
