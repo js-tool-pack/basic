82924f72d9c7f0ab333ff6e87829f8adac0b4b2b
dyh_a <xinzon32177@163.com>
2023-08-25 11:21:52 +0800

    feat(common): emptyFn 空函数

---
+++ src/common/emptyFn.ts
@@ -0,0 +1,7 @@
+/**
+ * 空函数，常用于用于默认值
+ * 比如在一些 eslint 禁止空函数的场景下使用
+ */
+export function emptyFn(): void {
+  // do nothing
+}
--- src/common/index.ts
+++ src/common/index.ts
@@ -10,3 +10,4 @@ export * from './getRoot';
 export * from './parseCmdParams';
 export * from './formatBytes';
 export * from './loadingElse';
+export * from './emptyFn';
