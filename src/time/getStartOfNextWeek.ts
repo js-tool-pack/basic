326138d761662f100e548e34c155beb0d067dad9
dyh_a <xinzon32177@163.com>
2023-12-16 18:49:27 +0800

    refactor(time): 使用 dateAdd 重构 getStartOfNextWeek

--- src/time/getStartOfNextWeek.ts
+++ src/time/getStartOfNextWeek.ts
@@ -1,4 +1,5 @@
 import { getStartOfWeek } from './getStartOfWeek';
+import { dateAdd } from './dateAdd';
 
 /**
  * 获取某日下个星期开始的date
@@ -21,7 +22,5 @@ import { getStartOfWeek } from './getStartOfWeek';
  * @param [weekBegin='monday'] 每个星期开始，可选'SunDay'|'MonDay' 周一 周日，默认周一
  */
 export function getStartOfNextWeek(date: Date, weekBegin: 'SunDay' | 'MonDay' = 'MonDay'): Date {
-  const d = getStartOfWeek(date, weekBegin);
-  d.setDate(d.getDate() + 7);
-  return d;
+  return dateAdd(getStartOfWeek(date, weekBegin), 1, 'week');
 }
