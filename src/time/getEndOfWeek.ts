ea1862cb9f9ee9455f666ac78a876e4de7874574
dyh_a <xinzon32177@163.com>
2023-12-19 17:54:34 +0800

    feat(time): getEndOfWeek 可选择星期的任意一天作为开始，不再局限于周一和周日
    BREAKING CHANGE: 移除 weekBegin 选项

--- src/time/getEndOfWeek.ts
+++ src/time/getEndOfWeek.ts
@@ -6,25 +6,29 @@ import { dateAdd } from './dateAdd';
  *
  * @example
  *
- * ```typescript
- * formatDate(getEndOfWeek(new Date('2023/04/19'))); // '2023-04-23 00:00:00'
- * formatDate(getEndOfWeek(new Date('2023/04/20'))); // '2023-04-23 00:00:00'
- * formatDate(getEndOfWeek(new Date('2023/04/16'))); // '2023-04-16 00:00:00'
- * formatDate(getEndOfWeek(new Date('2023/04/10'))); // '2023-04-16 00:00:00'
+ * const getEnd = (date: string, options?: Parameters<typeof getEndOfWeek>[1]) => formatDate(getEndOfWeek(new Date(date), options));
  *
- * formatDate(getEndOfWeek(new Date('2023/04/19'), 'SunDay')); // '2023-04-22 00:00:00'
- * formatDate(getEndOfWeek(new Date('2023/04/16'), 'SunDay')); // '2023-04-22 00:00:00'
- * formatDate(getEndOfWeek(new Date('2023/04/09'), 'SunDay')); // '2023-04-15 00:00:00'
- * formatDate(getEndOfWeek(new Date('2023/04/10'), 'SunDay')); // '2023-04-15 00:00:00'
- * ```
+ * // 星期一为星期第一天
+ * getEnd('2023/04/19', { firstDay: 1 }); // '2023-04-23 00:00:00'
+ * getEnd('2023/04/20', { firstDay: 1 }); // '2023-04-23 00:00:00'
+ * getEnd('2023/04/16', { firstDay: 1 }); // '2023-04-16 00:00:00'
+ * getEnd('2023/04/10', { firstDay: 1 }); // '2023-04-16 00:00:00'
+ *
+ * // 星期天为星期第一天
+ * getEnd('2023/04/19'); // '2023-04-22 00:00:00'
+ * getEnd('2023/04/16'); // '2023-04-22 00:00:00'
+ * getEnd('2023/04/09'); // '2023-04-15 00:00:00'
+ * getEnd('2023/04/10'); // '2023-04-15 00:00:00'
+ *
+ * @see getStartOfWeek
  *
  * @param date
- * @param [weekBegin='MonDay'] 每个星期开始，可选'SunDay'|'MonDay' 周一 周日，默认周一
+ * @param [firstDay=0] 每个星期的开始，可选0-7.默认0周日
+ * @param [weekOffset=0] 星期的偏移量，当值为 1 时是下个星期，为-1 时是上个星期以此类推.默认为 0
  */
-export function getEndOfWeek(date: Date, weekBegin: 'SunDay' | 'MonDay' = 'MonDay'): Date {
-  return dateAdd(
-    getStartOfWeek(date, { firstDay: weekBegin === 'SunDay' ? 0 : 1, weekOffset: 1 }),
-    -1,
-    'date',
-  );
+export function getEndOfWeek(
+  date: Date,
+  { firstDay = 0, weekOffset = 0 }: Parameters<typeof getStartOfWeek>[1] = {},
+): Date {
+  return dateAdd(getStartOfWeek(date, { firstDay, weekOffset: weekOffset + 1 }), -1, 'date');
 }
