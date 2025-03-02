d839084cfd81185290dfeec3247a63e5b0b3bdc1
dyh_a <xinzon32177@163.com>
2025-03-02 19:09:19 +0800

    docs(string/kebabCase): 修改文档

--- src/string/cases/kebabCase.ts
+++ src/string/cases/kebabCase.ts
@@ -6,8 +6,8 @@ import { splitByCases } from './splitByCases';
  * @example
  *
  * kebabCase('aBBcde-fFF__g   h'); // 'a-bbcde-f-ff-g-h'
- * kebabCase('APPStyle'); 'appstyle'
- * kebabCase('APP_Style'); 'app-style'
+ * kebabCase('APPStyle'); // 'appstyle'
+ * kebabCase('APP_Style'); // 'app-style'
  * kebabCase('abc0Abc0'); // 'abc0-abc0'
  * kebabCase('AUV'); // 'auv'
  * kebabCase(' Auv'); // 'auv'
